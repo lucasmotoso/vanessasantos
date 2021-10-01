cc891ac44dd004e92c76925ada6c14ee33d2dc8d
lucasmotoso <lucasmotoso@gmail.com>
2021-09-30 22:27:28 -0300

    Atualizado barras de navegação

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,7 @@
 			$(
 				'<div id="titleBar">' +
 					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
+					'<span class="title">' + 'Vanessa Santos - Seguros' + '</span>' +
 				'</div>'
 			)
 				.appendTo($body);
